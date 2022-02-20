f2f367bd78da5ed84cb333b5e30ba5e1d344bb97
NickZhang <maccus0329@gmail.com>
2022-02-20 12:15:08 +0800

    完成编辑页面，添加makrdown功能

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,16 +1,67 @@
 <template>
-  <div id="note-detail">
-    <h1>notebookId : {{ $route.query.notebookId }}</h1>
-    <h1>noteId : {{ $route.query.noteId }}</h1>
+  <div id="note" class="detail">
+    <note-sidebar @update:notes="val => (notes = val)"></note-sidebar>
+    <div class="note-detail">
+      <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
+      <div v-show="curNote.id" class="note-detail-ct">
+        <div class="note-bar">
+          <span>创建日期 {{ curNote.createdAtFriendly }}</span>
+          <span>更新日期{{ curNote.updatedAtFriendly }}</span>
+          <span>{{ statusText }}</span>
+          <span class="iconfont icon-delete" @click="deleteNote"></span>
+          <span
+            class="iconfont icon-fullscreen"
+            @click="isShowPreview = !isShowPreview"
+          ></span>
+        </div>
+        <div class="note-title">
+          <input
+            type="text"
+            placeholder="输入标题"
+            v-model="curNote.title"
+            @input="updateNote"
+            @keydown="statusText = '正在输入...'"
+          />
+        </div>
+        <div class="editor">
+          <textarea
+            placeholder="输入内容,支持markdown语法"
+            v-model="curNote.content"
+            @input="updateNote"
+            @keydown="statusText = '正在输入...'"
+            v-show="isShowPreview"
+          ></textarea>
+          <div
+            class="preview markdown-body"
+            v-html="previewContent"
+            v-show="!isShowPreview"
+          ></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Auth from "@/apis/auth";
+import Notes from "@/apis/notes";
+import _ from "lodash";
+import Bus from "@/helpers/bus";
+import NoteSidebar from "./NoteSidebar.vue";
+import MarkdownIt from "markdown-it";
+
+let md = new MarkdownIt();
+
 export default {
-  name: "Login",
+  components: {
+    NoteSidebar
+  },
   data() {
     return {
-      msg: "笔记详情页"
+      curNote: {},
+      notes: [],
+      statusText: "笔记未改动",
+      isShowPreview: false
     };
   },
   created() {
@@ -19,11 +70,51 @@ export default {
         this.$router.push({ path: "/login" });
       }
     });
+    Bus.$once("update:notes", val => {
+      this.curNote =
+        val.find(note => note.id == this.$route.query.noteId) || {};
+    });
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.curNote = this.notes.find(note => note.id == to.query.noteId) || {};
+    next();
+  },
+  methods: {
+    updateNote: _.debounce(function() {
+      Notes.updateNote(
+        { noteId: this.curNote.id },
+        { title: this.curNote.title, content: this.curNote.content }
+      )
+        .then(data => {
+          this.statusText = "已保存";
+          console.log(data);
+        })
+        .catch(data => {
+          this.statusText = "保存出错";
+        });
+    }, 300),
+    deleteNote() {
+      Notes.deleteNote({ noteId: this.curNote.id }).then(data => {
+        this.$message.success(data.msg);
+        this.notes.splice(this.notes.indexOf(this.curNote), 1);
+        this.$router.replace({ path: "/note" });
+      });
+    }
+  },
+  computed: {
+    previewContent() {
+      console.log(this.curNote.content || "");
+      return md.render(this.curNote.content || "");
+    }
   }
 };
 </script>
-<style scoped>
-h1 {
-  color: blue;
+<style lang="less" scoped>
+@import url(../assets/css/note-detail.less);
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
 }
 </style>
